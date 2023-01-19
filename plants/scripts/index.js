21a2916640a61ac83c3994110d035711a8367f00
a-reznikov <a.reznnikov@gmail.com>
2023-01-19 13:34:23 +0300

    feat: implement js script for burger

--- plants/scripts/index.js
+++ plants/scripts/index.js
@@ -1,1 +1,36 @@
-console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20")
+window.onload = function() {
+  console.log('Hello burger!');
+
+  addBurgerClickHandler();
+}
+
+const addBurgerClickHandler = () => {
+  document.querySelector('.hamburger').addEventListener('click', (e) => {
+    if (e.target.classList.contains('hamburger__opened')) {
+      console.log("Burger is opened");
+    } else {
+      openNavMenu();
+      transformeBurger();
+      console.log("Burger just open now");
+    }
+
+  })
+}
+
+const openNavMenu = () => {
+  let headerNav = document.querySelector('.header');
+  let nav = document.querySelector('.nav');
+  let navList = document.querySelector('.nav-list');
+  headerNav.classList.add('header__nav');
+  nav.classList.add('nav__opened');
+  navList.classList.add('nav-list-direction');
+}
+
+const transformeBurger = () => {
+  let hamburgerOpened = document.querySelector('.hamburger');
+  hamburgerOpened.classList.add('hamburger__opened');
+}
+
+
+
+// console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20")
