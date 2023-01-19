d6e1415b046570849cb1280a4f2a8d98a8a5acbf
a-reznikov <a.reznnikov@gmail.com>
2023-01-19 15:07:59 +0300

    refactor: change view burger menu

--- plants/scripts/index.js
+++ plants/scripts/index.js
@@ -7,10 +7,10 @@ window.onload = function() {
 const addBurgerClickHandler = () => {
   document.querySelector('.hamburger').addEventListener('click', (e) => {
     if (e.target.classList.contains('hamburger__opened')) {
+      closeNavMenu();
       console.log("Burger is opened");
     } else {
       openNavMenu();
-      transformeBurger();
       console.log("Burger just open now");
     }
 
@@ -21,15 +21,34 @@ const openNavMenu = () => {
   let headerNav = document.querySelector('.header');
   let nav = document.querySelector('.nav');
   let navList = document.querySelector('.nav-list');
+  let navItem = document.querySelectorAll('.nav-item');
+  let hamburgerOpened = document.querySelector('.hamburger');
+
   headerNav.classList.add('header__nav');
   nav.classList.add('nav__opened');
   navList.classList.add('nav-list-direction');
-}
-
-const transformeBurger = () => {
-  let hamburgerOpened = document.querySelector('.hamburger');
+  navItem.forEach(item => {
+    item.classList.add('nav-item-undercrossed');
+  })
   hamburgerOpened.classList.add('hamburger__opened');
 }
+
+const closeNavMenu = () => {
+  let headerNav = document.querySelector('.header');
+  let nav = document.querySelector('.nav');
+  let navList = document.querySelector('.nav-list');
+  let navItem = document.querySelectorAll('.nav-item');
+  let hamburgerOpened = document.querySelector('.hamburger');
+
+  headerNav.classList.remove('header__nav');
+  nav.classList.remove('nav__opened');
+  navList.classList.remove('nav-list-direction');
+  navItem.forEach(item => {
+    item.classList.remove('nav-item-undercrossed');
+  })
+  hamburgerOpened.classList.remove('hamburger__opened');
+}
+
 
 
 
