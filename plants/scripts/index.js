572e36250780ef32e4dde2a653d5d4716d6be25a
a-reznikov <a.reznnikov@gmail.com>
2023-01-20 14:12:57 +0300

    feat: update script for burger menu

--- plants/scripts/index.js
+++ plants/scripts/index.js
@@ -1,55 +1,9 @@
-window.onload = function() {
-  console.log('Hello burger!');
-
-  addBurgerClickHandler();
-}
-
-const addBurgerClickHandler = () => {
-  document.querySelector('.hamburger').addEventListener('click', (e) => {
-    if (e.target.classList.contains('hamburger__opened')) {
-      closeNavMenu();
-      console.log("Burger is opened");
-    } else {
-      openNavMenu();
-      console.log("Burger just open now");
-    }
-
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function() {
+    document.querySelector(".header").classList.toggle("header__opened");
+    document.querySelector(".nav").classList.toggle("nav__opened");
+    document.querySelector(".hamburger").classList.toggle("hamburger__opened");
   })
-}
+})
 
-const openNavMenu = () => {
-  let headerNav = document.querySelector('.header');
-  let nav = document.querySelector('.nav');
-  let navList = document.querySelector('.nav-list');
-  let navItem = document.querySelectorAll('.nav-item');
-  let hamburgerOpened = document.querySelector('.hamburger');
-
-  headerNav.classList.add('header__nav');
-  nav.classList.add('nav__opened');
-  navList.classList.add('nav-list-direction');
-  navItem.forEach(item => {
-    item.classList.add('nav-item-undercrossed');
-  })
-  hamburgerOpened.classList.add('hamburger__opened');
-}
-
-const closeNavMenu = () => {
-  let headerNav = document.querySelector('.header');
-  let nav = document.querySelector('.nav');
-  let navList = document.querySelector('.nav-list');
-  let navItem = document.querySelectorAll('.nav-item');
-  let hamburgerOpened = document.querySelector('.hamburger');
-
-  headerNav.classList.remove('header__nav');
-  nav.classList.remove('nav__opened');
-  navList.classList.remove('nav-list-direction');
-  navItem.forEach(item => {
-    item.classList.remove('nav-item-undercrossed');
-  })
-  hamburgerOpened.classList.remove('hamburger__opened');
-}
-
-
-
-
-// console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20")
+// // console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20")
