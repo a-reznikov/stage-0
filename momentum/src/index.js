a588ad932f2e6623048dfc4457e8971869cd7049
a-reznikov <a.reznnikov@gmail.com>
2023-02-15 15:46:19 +0300

    feat: add time and date

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -2,3 +2,18 @@
 
 //time(1, 2);
 console.log("test");
+const time = document.querySelector('.time');
+const dateFull = document.querySelector('.date');
+
+function showTime() {
+  const date = new Date();
+  console.log(date);
+  const currentTime = date.toLocaleTimeString();
+  const options = {month: 'long', day: 'numeric'};
+  const currentDate = date.toLocaleDateString('en-En', options);
+  time.textContent = currentTime;
+  dateFull.textContent = currentDate;
+  setTimeout(showTime, 1000);
+}
+
+showTime();
