6bb25433c2757a914ad07a0f3912c1b8f0c5641a
a-reznikov <a.reznnikov@gmail.com>
2023-02-16 12:24:33 +0300

    feat: add time and greeting

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,19 +1,57 @@
 //import { time } from 'scripts/time';
 
-//time(1, 2);
+//Time and Date
 console.log("test");
 const time = document.querySelector('.time');
 const dateFull = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const inputName = document.querySelector('.name');
 
 function showTime() {
   const date = new Date();
-  console.log(date);
   const currentTime = date.toLocaleTimeString();
-  const options = {month: 'long', day: 'numeric'};
+  const options = {weekday: 'long', month: 'long', day: 'numeric'};
   const currentDate = date.toLocaleDateString('en-En', options);
   time.textContent = currentTime;
-  dateFull.textContent = currentDate;
+  dateFull.textContent =  currentDate;
+  getTimeOfDay();
   setTimeout(showTime, 1000);
 }
 
-showTime();
+showTime()
+
+//Greeting
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  let dayPart = Math.floor(hours / 6);
+  let timeOfDay = '';
+  switch(dayPart) {
+    case 1:  timeOfDay = "morning ";
+      break;
+    case 2:  timeOfDay = "afternoon";
+      break;
+    case 3:  timeOfDay = "evening";
+      break;
+    case 0:  timeOfDay = "night";
+      break;
+  }
+  greeting.textContent = `Good ${timeOfDay}`;
+}
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    inputName.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage);
+
+function setLocalStorage() {
+  localStorage.setItem('name', inputName.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+
+//Slider
+
+
